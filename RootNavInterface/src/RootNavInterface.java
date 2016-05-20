7d59c408e90952cfda7efc43a38361b39f5071ca
Tuan <tuan@debianV2>
2016-05-20 20:10:49 +0100

    Added logs (readonly) and formated doc

--- RootNavInterface/src/RootNavInterface.java
+++ RootNavInterface/src/RootNavInterface.java
@@ -1,7 +1,7 @@
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.io.BufferedReader;
@@ -9,54 +9,51 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.regex.Pattern;
 
+import javax.swing.BoxLayout;
 import javax.swing.JApplet;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JTextField;
 import javax.swing.JTextArea;
-import java.awt.Color;
-import javax.script.*;
-import javax.swing.BoxLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-
 
 public class RootNavInterface extends JApplet {
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1686644263900653234L;
 	protected static int height = 640;
 	protected static int width = 800;
 
 	protected static String STAGING_FOLDER_ROOT = "/home/tuan/staging/";
-	
+
 	private Image img;
 	private MediaTracker tr;
-	private JTextField txtCurrentPath;
+	
+	// private JTextField txtCurrentPath;
 	// private MainFrame mainFrame;
-	private JTextArea textArea = new JTextArea();
+	// private JTextArea textArea = new JTextArea();
+	
 	private JTextArea txtLogArea = new JTextArea();
-	
+
 	private String[] args;
-	
+
 	private String mex;
 	private String staging;
 	private String stagingParentFolder;
 	private String fullStagingFolder;
-	private String imgurl ;
-	
+	private String imgurl;
+
 	private String resultFile;
-	
-	
+
 	public RootNavInterface() {
 		this(null);
-		
-		
+
 	}
 
 	/**
@@ -64,30 +61,34 @@ public class RootNavInterface extends JApplet {
 	 */
 	public RootNavInterface(String args[]) {
 		this.args = args;
-		
-		
+
 	}
 
-//	public void paint(Graphics g) {
-//		tr = new MediaTracker(this);
-		// just for testing
-		
-		//img = getImage(getCodeBase(), "0002.jpg.tif");
-		//img = getImage(getCodeBase(), "/home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
-//		try {
-			//img = getImage(new URL("file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg"));
-			//img = getImage(getDocumentBase(), "file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
-		//img = getImage(getCodeBase(), "file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
-		//img = getImage(getCodeBase(), "file:///home/tuan/staging/00-4NWciXLTvhKnd2LYVYedVj/0002.jpg");
-				
-		//	tr.addImage(img, 0);
-	//		g.drawImage(img, 0, 0, this);
-		//} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-	//	}
-	
-//	}
+	// public void paint(Graphics g) {
+	// tr = new MediaTracker(this);
+	// just for testing
+
+	// img = getImage(getCodeBase(), "0002.jpg.tif");
+	// img = getImage(getCodeBase(),
+	// "/home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
+	// try {
+	// img = getImage(new
+	// URL("file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg"));
+	// img = getImage(getDocumentBase(),
+	// "file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
+	// img = getImage(getCodeBase(),
+	// "file:///home/tuan/bisque/modules/RootNavLinuxModuleV3/0002.jpg");
+	// img = getImage(getCodeBase(),
+	// "file:///home/tuan/staging/00-4NWciXLTvhKnd2LYVYedVj/0002.jpg");
+
+	// tr.addImage(img, 0);
+	// g.drawImage(img, 0, 0, this);
+	// } catch (MalformedURLException e) {
+	// TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+
+	// }
 
 	private void initGUI() {
 		Object f = getParent();
@@ -97,9 +98,9 @@ public class RootNavInterface extends JApplet {
 		}
 
 		((Frame) f).setTitle("RootNav Interface");
-		
+
 		setSize(new Dimension(width, height));
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 
@@ -125,26 +126,25 @@ public class RootNavInterface extends JApplet {
 		menuBar.add(mnHelp);
 
 		JMenuItem mntmAbout = new JMenuItem("About");
-		mnHelp.add(mntmAbout);		
-		
+		mnHelp.add(mntmAbout);
+
 		JPanel pnlMainPanel = new JPanel();
 		getContentPane().add(pnlMainPanel, BorderLayout.CENTER);
-		
-		txtCurrentPath = new JTextField();
-		//pnlMainPanel.add(txtCurrentPath);
-		txtCurrentPath.setColumns(10);
-		
-		
-		textArea.setColumns(10);
-		textArea.setRows(20);
-		textArea.setBackground(Color.GRAY);
-		//pnlMainPanel.add(textArea);
-		
+
+		// txtCurrentPath = new JTextField();
+		// pnlMainPanel.add(txtCurrentPath);
+		// txtCurrentPath.setColumns(10);
+
+		// textArea.setColumns(10);
+		// textArea.setRows(20);
+		// textArea.setBackground(Color.GRAY);
+		// pnlMainPanel.add(textArea);
+
 		JPanel pnlLog = new JPanel();
 		getContentPane().add(pnlLog, BorderLayout.SOUTH);
 		pnlLog.setLayout(new BoxLayout(pnlLog, BoxLayout.X_AXIS));
-		
-		
+		txtLogArea.setEditable(false);
+
 		txtLogArea.setRows(10);
 		pnlLog.add(txtLogArea);
 
@@ -152,163 +152,194 @@ public class RootNavInterface extends JApplet {
 
 	public void init() {
 		initGUI();
-		
-		//readFile();
-		if (args != null)
-		{
+
+		// readFile();
+		if (args != null) {
 			StringBuffer strBuff = new StringBuffer();
-			
+
 			for (String string : args) {
 				strBuff.append(string + "\n");
 			}
-			
-			this.txtCurrentPath.setText(strBuff.toString());
-		}
-		else
-		{
-			
-			
+
+			// this.txtCurrentPath.setText(strBuff.toString());
+			WriteLog(strBuff.toString());
+		} else {
+
 			this.mex = this.getParameter("mex");
-			//get image file
+			// get image file
 			this.imgurl = this.getParameter("image_url");
 			this.stagingParentFolder = this.getParameter("stagingFolder");
-			
-			if (mex != null)
-			{
+
+			if (mex != null) {
 				String[] parts = this.mex.split("/");
-				
-				this.staging = parts[parts.length - 1]; //assume the staging is the last part of the string
-						
+
+				this.staging = parts[parts.length - 1]; // assume the staging is
+														// the last part of the
+														// string
+
 				this.fullStagingFolder = this.stagingParentFolder + this.staging;
-				
-				//this.txtCurrentPath.setText("mex: " + mex + "\n" +  "Staging: " + this.staging);
-				
-				
-				/*if (imgurl != null)
-					this.txtCurrentPath.setText("image_url: " + imgurl);
-				else
-					this.txtCurrentPath.setText("image_url: Dont know." );*/
-							
-				
-				
+
+				// this.txtCurrentPath.setText("mex: " + mex + "\n" + "Staging:
+				// " + this.staging);
+
+				/*
+				 * if (imgurl != null) this.txtCurrentPath.setText("image_url: "
+				 * + imgurl); else this.txtCurrentPath.setText(
+				 * "image_url: Dont know." );
+				 */
+
 			}
-			/*else 
-				this.txtCurrentPath.setText("No parameters");
-			
-			
-			String resouce = this.getParameter("resourcename");
-						
-			String inputText = this.getParameter("inputText");
-			String resourceType = this.getParameter("resourceType");
-			String nameInput = this.getParameter("nameInput");
-			
-			
-			this.textArea.setText("Resouce: " + resouce + "\n" + 
-			"Input text: " + inputText + 
-			"\nResource type: " + resourceType +
-			"\nameInput: " + nameInput);*/
-			
-			this.textArea.setText("URL: " + imgurl + "\n" + 
-					"Staging Parent folder: " + stagingParentFolder + 
-					"\nFull Staging folder: " + fullStagingFolder );
 			/*
-			URL location = RootNavInterface.class.getProtectionDomain().getCodeSource().getLocation();
-			
-			this.txtCurrentPath.setText("location: " + location.getFile());*/
-			
+			 * else this.txtCurrentPath.setText("No parameters");
+			 * 
+			 * 
+			 * String resouce = this.getParameter("resourcename");
+			 * 
+			 * String inputText = this.getParameter("inputText"); String
+			 * resourceType = this.getParameter("resourceType"); String
+			 * nameInput = this.getParameter("nameInput");
+			 * 
+			 * 
+			 * this.textArea.setText("Resouce: " + resouce + "\n" +
+			 * "Input text: " + inputText + "\nResource type: " + resourceType +
+			 * "\nameInput: " + nameInput);
+			 */
+
+			/*
+			 * this.textArea.setText("URL: " + imgurl + "\n" +
+			 * "Staging Parent folder: " + stagingParentFolder +
+			 * "\nFull Staging folder: " + fullStagingFolder );
+			 */
+
+			WriteLog("URL: " + imgurl + "\n" + "Staging Parent folder: " + stagingParentFolder
+					+ "\nFull Staging folder: " + fullStagingFolder);
+
+			/*
+			 * URL location =
+			 * RootNavInterface.class.getProtectionDomain().getCodeSource().
+			 * getLocation();
+			 * 
+			 * this.txtCurrentPath.setText("location: " + location.getFile());
+			 */
+
 			// create a script engine manager
-	        /*ScriptEngineManager factory = new ScriptEngineManager();
-	        // create a JavaScript engine
-	        ScriptEngine engine = factory.getEngineByName("JavaScript");
-	        // evaluate JavaScript code from String
-	        try {
-				engine.eval(" BQFactory.request({ uri: String(myoutputImg)," + 
-								"cb: callback(this, 'onResource'), " + 
-								"errorcb: callback(this, 'onerror'), " + 
-								//uri_params: {view:'short'}, // dima: by default it's short, if error happens we try to mark that in the list by fetched url
-								"}); " );
-				//get script object
-				engine.eval(" var obj = BQFactory.session[" + imgurl + "];" );
-				
-				Object obj = engine.get("obj");
-				
-				this.txtCurrentPath.setText(obj.toString());
-				 
-			} catch (ScriptException e) {
-				// TODO Auto-generated catch block
-				this.txtCurrentPath.setText(e.getMessage());
-			}*/
-			
-			WriteLog("Beginning to find files");
-			
+			/*
+			 * ScriptEngineManager factory = new ScriptEngineManager(); //
+			 * create a JavaScript engine ScriptEngine engine =
+			 * factory.getEngineByName("JavaScript"); // evaluate JavaScript
+			 * code from String try { engine.eval(
+			 * " BQFactory.request({ uri: String(myoutputImg)," +
+			 * "cb: callback(this, 'onResource'), " +
+			 * "errorcb: callback(this, 'onerror'), " + //uri_params:
+			 * {view:'short'}, // dima: by default it's short, if error happens
+			 * we try to mark that in the list by fetched url "}); " ); //get
+			 * script object engine.eval(" var obj = BQFactory.session[" +
+			 * imgurl + "];" );
+			 * 
+			 * Object obj = engine.get("obj");
+			 * 
+			 * this.txtCurrentPath.setText(obj.toString());
+			 * 
+			 * } catch (ScriptException e) { // TODO Auto-generated catch block
+			 * this.txtCurrentPath.setText(e.getMessage()); }
+			 */
+
+			WriteLog("Begin finding xml result files");
+
 			String resultPattern = ".*_result.xml";
-			
-			File[] selectedFiles = listFilesMatching(new File("/home/tuan/staging/00-NJqZATtwSAezwX2o53oGyc"), resultPattern);
-			
-			if (selectedFiles.length > 0)
-			{
-				this.txtCurrentPath.setText(selectedFiles[0].getName());
-			} else
-			{
-				this.txtCurrentPath.setText("No files found. Check again");
+
+			File[] selectedFiles = listFilesMatching(new File("/home/tuan/staging/00-NJqZATtwSAezwX2o53oGyc"),
+					resultPattern);
+			// File[] selectedFiles = listFilesMatching(new
+			// File(this.fullStagingFolder), resultPattern);
+
+			selectedFiles = sortByLastModifiedDate(selectedFiles);
+
+			if (selectedFiles.length > 0) {
+				this.resultFile = selectedFiles[0].getName();
+				
+				WriteLog("Selected file: " + selectedFiles[0].getName());
+				// this.txtCurrentPath.setText(selectedFiles[0].getName());
+			} else {
+				WriteLog("No file selected");
+				// this.txtCurrentPath.setText("No files found. Check again");
 			}
-			
-			WriteLog("End to find files");
+
+			WriteLog("End finding files");
 		}
 	}
-	
-	public void readFile()
-	{
+
+	public void readFile() {
 		String line;
 		// /home/tuan/staging/00-KyhsZw8coxQV7C6s4CnVzj/0002.jpg.tif_result.xml
-		//String filename = "/home/tuan/staging/00-KyhsZw8coxQV7C6s4CnVzj/0002.jpg.tif_result.xml";
+		// String filename =
+		// "/home/tuan/staging/00-KyhsZw8coxQV7C6s4CnVzj/0002.jpg.tif_result.xml";
 		String filename = "file:///home/tuan/staging/00-VobuFrwbUHvrAoRLonNc7V/InputData.xml";
 		String prHtml = this.getParameter("filename");
 		URL url = null;
-		
-		try{
+
+		try {
 			url = new URL(filename);
 			InputStream in = url.openStream();
 			BufferedReader bf = new BufferedReader(new InputStreamReader(in));
-			
+
 			StringBuffer strBuff = new StringBuffer();
-			
-			while((line = bf.readLine()) != null)
-			{
+
+			while ((line = bf.readLine()) != null) {
 				strBuff.append(line + "\n");
 			}
-			
-			this.txtCurrentPath.setText(strBuff.toString());
-		}
-		catch(Exception e)
-		{
-			this.txtCurrentPath.setText("Problem: " + e.getMessage());
+
+			// this.txtCurrentPath.setText(strBuff.toString());
+			WriteLog(strBuff.toString());
+		} catch (Exception e) {
+			// this.txtCurrentPath.setText("Problem: " + e.getMessage());
+			WriteLog("Problem: " + e.getMessage());
 		}
 	}
-	private void WriteLog(String text)
-	{
+
+	private void WriteLog(String text) {
 		this.txtLogArea.append(text);
 		this.txtLogArea.append("\n");
-		
+
 	}
+
 	public static File[] listFilesMatching(File root, String regex) {
-	    if(!root.isDirectory()) {
-	        throw new IllegalArgumentException(root+" is no directory.");
-	    }
-	    final Pattern p = Pattern.compile(regex); // careful: could also throw an exception!
-	    //return root.listFiles();
-	    return root.listFiles(new FileFilter(){
-	        @Override
-	        public boolean accept(File file) {
-	        	return p.matcher(file.getName()).matches();
-	        }
-	    });
+		if (!root.isDirectory()) {
+			throw new IllegalArgumentException(root + " is no directory.");
+		}
+		final Pattern p = Pattern.compile(regex); // careful: could also throw
+													// an exception!
+		// return root.listFiles();
+		return root.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return p.matcher(file.getName()).matches();
+			}
+		});
 	}
-	
+
+	public static File[] sortByLastModifiedDate(File[] files) {
+		Arrays.sort(files, new Comparator<Object>() {
+			public int compare(Object o1, Object o2) {
+
+				if (((File) o1).lastModified() > ((File) o2).lastModified()) {
+					return -1;
+				} else if (((File) o1).lastModified() < ((File) o2).lastModified()) {
+					return +1;
+				} else {
+					return 0;
+				}
+			}
+
+		});
+
+		return files;
+	}
+
 	public static void main(String[] args) {
 
 		MainFrame frame = new MainFrame(args);
-		
+
 		frame.setVisible(true);
 
 	}
