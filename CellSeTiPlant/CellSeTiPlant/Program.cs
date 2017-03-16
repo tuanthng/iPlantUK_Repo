d88c3cd30ddd3c0b5d0da3fdb51a7d8d6891cfca
Tuan Nguyen <tuanthng@gmail.com>
2017-03-16 17:46:52 +0000

    Initialsed CellSet for iPlant

---
+++ CellSeTiPlant/CellSeTiPlant/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Plossum.CommandLine;
+
+namespace CellSeTiPlant
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("CellSetiPlant started!");
+
+			int argc = args.Length;
+
+
+		}
+	}
+}
